f0b8465647c768bfee58533ccc96ee221b71cdd2
Jxnxrd <143683501+SendHelp001@users.noreply.github.com>
2024-09-26 07:56:29 +0800

    Creation of my branch
    added some test sprites as

--- moonless_project/scenes/main_character.gd
+++ moonless_project/scenes/main_character.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@onready var sprite_2d = $Sprite2D
+@onready var sprite_2d = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
